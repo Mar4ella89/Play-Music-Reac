c8d54a24c1e62d2d221056848f9ef502ab8ecccb
Marat Baikin <3371335@gmail.com>
2023-04-20 17:31:04 +0300

    upd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,17 @@
+import css from './App.module.css';
+
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+  const imageUrl =
+    'https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640';
+  const productPrice = 10.99;
+  const product = (
+    <div className={css.container}>
+      <img src={imageUrl} alt="Tacos With Lime" width="640" />
+      <h2>Tacos With Lime</h2>
+      <p>Price: {productPrice}$</p>
+      <button type="button">Add to cart</button>
     </div>
   );
+
+  return <>{product}</>;
 };
