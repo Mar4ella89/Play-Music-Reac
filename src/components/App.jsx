0f0d955cf740b90c009b331bc95ebb054cdd4937
Marat Baikin <3371335@gmail.com>
2023-04-25 18:01:21 +0300

    upd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,18 @@
+import Product from './Product';
 import css from './App.module.css';
 
-export const App = () => {
-  const imageUrl =
-    'https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640';
-  const productPrice = 10.99;
-  const product = (
-    <div className={css.container}>
-      <img src={imageUrl} alt="Tacos With Lime" width="640" />
-      <h2>Tacos With Lime</h2>
-      <p>Price: {productPrice}$</p>
-      <button type="button">Add to cart</button>
-    </div>
-  );
-
-  return product;
-};
+export const App = () => (
+  <div>
+    <h1>Best selling products</h1>
+    <Product
+      imgUrl="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640"
+      name="Tacos With Lime"
+      price={10.99}
+    />
+    <Product
+      imgUrl="https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg?dpr=2&h=480&w=640"
+      name="Fries and Burger"
+      price={14.29}
+    />
+  </div>
+);
