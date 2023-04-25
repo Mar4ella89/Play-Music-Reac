4ea1175fecaa91a93951a27d22791b5865a2a468
Marat Baikin <3371335@gmail.com>
2023-04-25 18:02:24 +0300

    Update Product.jsx

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -2,7 +2,12 @@ import { render } from '@testing-library/react';
 import React from 'react';
 
 const Product = () => {
-  return ()
+  return (<div>
+    <img src={imgUrl} alt={name} width="640" />
+    <h2>{name}</h2>
+    <p>Price: {price}$</p>
+    <button type="button">Add to cart</button>
+  </div>;)
 };
 
 export default Product;
