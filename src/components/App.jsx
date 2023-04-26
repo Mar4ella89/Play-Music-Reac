a93aca62d2c4a5adcf231d39c8fc8f0a6ae52a19
Marat Baikin <3371335@gmail.com>
2023-04-26 12:43:59 +0300

    upd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,7 @@ import Product from './Product';
 import css from './App.module.css';
 
 export const App = () => (
-  <div>
+  <div className={css.container}>
     <h1>Best selling products</h1>
     <Product
       imgUrl="https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640"
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,13 +1,16 @@
-import { render } from '@testing-library/react';
 import React from 'react';
 
-const Product = () => {
-  return (<div>
-    <img src={imgUrl} alt={name} width="640" />
-    <h2>{name}</h2>
-    <p>Price: {price}$</p>
-    <button type="button">Add to cart</button>
-  </div>;)
+import css from './Product.module.css';
+
+const Product = ({ name, imgUrl, price }) => {
+  return (
+    <div className={css.card}>
+      <img src={imgUrl} alt={name} width="640" />
+      <h2>{name}</h2>
+      <p>Price: {price}$</p>
+      <button type="button">Add to cart</button>
+    </div>
+  );
 };
 
 export default Product;
