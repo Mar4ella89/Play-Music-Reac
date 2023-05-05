b458821ac4e3d0cc7d11110d8aa9bca4ab32f1d6
Marat Baikin <3371335@gmail.com>
2023-05-05 16:44:06 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
+
 import { ThemeProvider } from '@emotion/react';
 import './index.css';
 import { App } from 'components/App/App';
