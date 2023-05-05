247b6baf67fdfec74c649c961fad1e4d4f806138
Marat Baikin <3371335@gmail.com>
2023-05-05 16:18:01 +0300

    added

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,11 @@
-import Product from './Products/Product';
-import Section from './Section/Section';
+import Product from '../Products/Product';
+import Section from '../Section/Section';
 import css from './App.module.css';
+
+import { PageTitle } from 'components/PageTitle/PageTitle';
+import { EventBoard } from 'components/EventBoard/EventBoard';
+import upcomingEvents from '../../upcoming-events.json';
+import { Container } from './App.styled';
 
 const allFood = [
   {
@@ -26,11 +31,15 @@ const allFood = [
 ];
 
 export const App = () => (
-  <div className={css.container}>
-    <h1>Best selling products</h1>
-    <Product allFood={allFood} />
-    <Section title={'TOP 3'} />
-    <Section />
-    <Section title={'NEW 3'} />
-  </div>
+  <Container>
+    <div className={css.container}>
+      <h1>Best selling products</h1>
+      <Product allFood={allFood} />
+      <Section title={'TOP 3'} />
+      <Section />
+      <Section title={'NEW 3'} />
+    </div>
+    <PageTitle text="24th Core Worlds Coalition Conference" />
+    <EventBoard events={upcomingEvents} />
+  </Container>
 );
