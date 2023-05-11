2eb44fbf0339c9bacecf74419b04b840e1d9298a
Marat Baikin <3371335@gmail.com>
2023-05-11 15:01:36 +0300

    Update SignUpFormFull.jsx

--- src/Form-example-4/SignUpFormFull.jsx
+++ src/Form-example-4/SignUpFormFull.jsx
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
+import { nanoid } from 'nanoid';
+
+const Gender = {
+  MALE: 'male',
+  FEMALE: 'female',
+};
 
 const INITIAL_STATE = {
   login: '',
   email: '',
   password: '',
+  agreed: false,
+  gender: null,
+  age: '',
 };
 
 class SignUpFormFull extends Component {
@@ -11,20 +20,23 @@ class SignUpFormFull extends Component {
     ...INITIAL_STATE,
   };
 
+  loginInputId = nanoid();
+
   // Отвечает за обновление состояния
   handleChange = e => {
-    const { name, value } = e.target;
-    this.setState({ [name]: value });
-    console.dir(e.target);
-    console.log(e.target.name);
-    console.log(e.target.value);
+    const { name, value, type, checked } = e.target;
+    // Если тип элемента checkbox, берем значение checked,
+    // в противном случае value
+    this.setState({ [name]: type === 'checkbox' ? checked : value });
   };
 
   // Вызывается при отправке формы
   handleSubmit = evt => {
-    const { login, email, password } = this.state;
+    const { login, email, password, agreed, age } = this.state;
     evt.preventDefault();
-    console.log(`Login: ${login}, Email: ${email}, Password: ${password}`);
+    console.log(
+      `Login: ${login}, Email: ${email}, Password: ${password}, Agreed: ${agreed}, Age: ${age}`
+    );
 
     // Проп который передается форме для вызова при сабмите
     this.props.onSubmit({ ...this.state });
@@ -36,13 +48,14 @@ class SignUpFormFull extends Component {
   };
 
   render() {
-    const { login, email, password } = this.state;
+    const { login, email, password, agreed, gender, age } = this.state;
 
     return (
       <form onSubmit={this.handleSubmit}>
-        <label>
+        <label htmlFor={this.loginInputId}>
           Login
           <input
+            id={this.loginInputId}
             type="text"
             placeholder="Enter login"
             name="login"
@@ -70,8 +83,55 @@ class SignUpFormFull extends Component {
             onChange={this.handleChange}
           />
         </label>
+        <label>
+          Agree to terms
+          <input
+            type="checkbox"
+            name="agreed"
+            checked={agreed}
+            onChange={this.handleChange}
+          />
+        </label>
 
-        <button type="submit">Sign up as {login}</button>
+        <section>
+          <h2>Choose your gender</h2>
+          <label>
+            Male
+            <input
+              type="radio"
+              checked={gender === Gender.MALE}
+              name="gender"
+              value={Gender.MALE}
+              onChange={this.handleChange}
+            />
+          </label>
+          <label>
+            Female
+            <input
+              type="radio"
+              checked={gender === Gender.FEMALE}
+              name="gender"
+              value={Gender.FEMALE}
+              onChange={this.handleChange}
+            />
+          </label>
+        </section>
+
+        <label>
+          Choose your age
+          <select name="age" value={age} onChange={this.handleChange}>
+            <option value="" disabled>
+              ...
+            </option>
+            <option value="18-25">18-25</option>
+            <option value="26-35">26-35</option>
+            <option value="36+">36+</option>
+          </select>
+        </label>
+
+        <button type="submit" disabled={!agreed}>
+          Sign up as {login}
+        </button>
       </form>
     );
   }
