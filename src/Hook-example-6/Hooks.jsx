c648437fc1b12f3467740e97c1947e3a20dfa7f2
Marat Baikin <3371335@gmail.com>
2023-05-31 17:43:09 +0300

    Create Hooks.jsx

---
+++ src/Hook-example-6/Hooks.jsx
@@ -0,0 +1,18 @@
+import { useState, useEffect } from 'react';
+
+const Hooks = () => {
+  const [value, setValue] = useState(0);
+
+  useEffect(() => {
+    document.title = `You clicked ${value} times`;
+  });
+
+  return (
+    <div>
+      <p>You clicked {value} times</p>
+      <button onClick={() => setValue(value + 1)}>Click me</button>
+    </div>
+  );
+};
+
+export default Hooks;
