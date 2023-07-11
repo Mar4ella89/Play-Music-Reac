8f885fb0cd3ffbd5256f382e257116d99ac7ebbe
Marat Baikin <3371335@gmail.com>
2023-07-11 17:47:54 +0300

    complited status filter

--- src/AllExamples/Redux-example-8/components/StatusFilter/StatusFilter.js
+++ src/AllExamples/Redux-example-8/components/StatusFilter/StatusFilter.js
@@ -1,12 +1,19 @@
+import { useSelector } from 'react-redux';
+
+import { statusFilters } from 'AllExamples/Redux-example-8/redux/constants';
 import { Button } from '../Button/Button';
+
 import css from './StatusFilter.module.css';
 
 export const StatusFilter = () => {
+  // Получаем значение фильтра из состояния Redux
+  const filter = useSelector(state => state.filters.status);
+
   return (
     <div className={css.wrapper}>
-      <Button>All</Button>
-      <Button>Active</Button>
-      <Button>Completed</Button>
+      <Button selected={filter === statusFilters.all}>All</Button>
+      <Button selected={filter === statusFilters.active}>Active</Button>
+      <Button selected={filter === statusFilters.completed}>Completed</Button>
     </div>
   );
 };
