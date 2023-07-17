a653a8a77a5fede4f7b89cef730aac9f31a1ddf6
Marat Baikin <3371335@gmail.com>
2023-07-17 17:37:13 +0300

    Update actions.js

--- src/AllExamples/Redux-Toolkit-example-9/redux/actions.js
+++ src/AllExamples/Redux-Toolkit-example-9/redux/actions.js
@@ -1,33 +1,6 @@
-import { nanoid } from 'nanoid';
+import { createAction } from '@reduxjs/toolkit';
 
-export const addTask = text => {
-  return {
-    type: 'tasks/addTask',
-    payload: {
-      id: nanoid(),
-      text,
-      completed: false,
-    },
-  };
-};
-
-export const deleteTask = taskId => {
-  return {
-    type: 'tasks/deleteTask',
-    payload: taskId,
-  };
-};
-
-export const toggleCompleted = taskId => {
-  return {
-    type: 'tasks/toggleCompleted',
-    payload: taskId,
-  };
-};
-
-export const setStatusFilter = value => {
-  return {
-    type: 'filters/setStatusFilter',
-    payload: value,
-  };
-};
+export const addTask = createAction('tasks/addTask');
+export const deleteTask = createAction('tasks/deleteTask');
+export const toggleCompleted = createAction('tasks/toggleCompleted');
+export const setStatusFilter = createAction('filters/setStatusFilter');
