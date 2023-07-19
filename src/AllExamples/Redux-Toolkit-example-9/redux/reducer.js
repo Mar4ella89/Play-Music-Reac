f26066832ff171203b21da9ed071261c3decb145
Marat Baikin <3371335@gmail.com>
2023-07-19 18:02:07 +0300

    Update reducer.js

--- src/AllExamples/Redux-Toolkit-example-9/redux/reducer.js
+++ src/AllExamples/Redux-Toolkit-example-9/redux/reducer.js
@@ -1,7 +1,12 @@
 import { createReducer } from '@reduxjs/toolkit';
 
 import { statusFilters } from './constants';
-import { addTask, deleteTask, toggleCompleted, setStatusFilter } from './actions';
+import {
+  addTask,
+  deleteTask,
+  toggleCompleted,
+  setStatusFilter,
+} from './actions';
 import { nanoid } from 'nanoid';
 
 const tasksInitialState = [
@@ -11,43 +16,57 @@ const tasksInitialState = [
   { id: 3, text: 'Discover Redux', completed: false },
   { id: 4, text: 'Build amazing apps', completed: false },
 ];
+
+// C Библиотека Immer
+
 export const tasksReducer = createReducer(tasksInitialState, {
   [addTask]: (state, action) => {
-    return [...state, action.payload];
+    state.push(action.payload);
   },
   [deleteTask]: (state, action) => {
-    return state.filter(task => task.id !== action.payload);
+    const index = state.findIndex(task => task.id === action.payload);
+    state.splice(index, 1);
   },
   [toggleCompleted]: (state, action) => {
-    return state.map(task => {
-      if (task.id !== action.payload) {
-        return task;
+    for (const task of state) {
+      if (task.id === action.payload) {
+        task.completed = !task.completed;
+        break;
       }
-      return { ...task, completed: !task.completed };
-    });
+    }
   },
 });
 
-
+// export const tasksReducer = createReducer(tasksInitialState, {
+//   [addTask]: (state, action) => {
+//     return [...state, action.payload];
+//   },
+//   [deleteTask]: (state, action) => {
+//     return state.filter(task => task.id !== action.payload);
+//   },
+//   [toggleCompleted]: (state, action) => {
+//     return state.map(task => {
+//       if (task.id !== action.payload) {
+//         return task;
+//       }
+//       return { ...task, completed: !task.completed };
+//     });
+//   },
+// });
 
 const filtersInitialState = {
   status: statusFilters.all,
 };
 
+// C Библиотека Immer
 export const filtersReducer = createReducer(filtersInitialState, {
   [setStatusFilter]: (state, action) => {
-    return { ...state, status: action.payload };
-  }
+    state.status = action.payload;
+  },
 });
 
-// export const filtersReducer = (state = filtersInitialState, action) => {
-//   switch (action.type) {
-//     case 'filters/setStatusFilter':
-//       return {
-//         ...state,
-//         status: action.payload,
-//       };
-//     default:
-//       return state;
-//   }
-// };
+// export const filtersReducer = createReducer(filtersInitialState, {
+//   [setStatusFilter]: (state, action) => {
+//     return { ...state, status: action.payload };
+//   },
+// });
