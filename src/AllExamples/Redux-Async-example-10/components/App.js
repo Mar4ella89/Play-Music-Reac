8fa7392766b0267a601051172b57f1014584cb7b
Marat Baikin <3371335@gmail.com>
2023-07-22 13:15:53 +0300

    updated slice

--- src/AllExamples/Redux-Async-example-10/components/App.js
+++ src/AllExamples/Redux-Async-example-10/components/App.js
@@ -2,7 +2,7 @@ import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 import { fetchTasks } from '../redux/operations';
-import { getTasks } from '../redux/selectors';
+import { getTasks, getIsLoading, getError } from '../redux/selectors';
 
 import { Layout } from './Layout/Layout';
 import { AppBar } from './AppBar/AppBar';
@@ -12,24 +12,26 @@ import { TaskList } from './TaskList/TaskList';
 const App = () => {
   const dispatch = useDispatch();
   // Получаем части состояния
-  const { items, isLoading, error } = useSelector(getTasks);
+  const tasks = useSelector(getTasks);
+  const isLoading = useSelector(getIsLoading);
+  const error = useSelector(getError);
   // Вызываем операцию
   useEffect(() => {
     dispatch(fetchTasks());
   }, [dispatch]);
 
-  console.log(items);
+  console.log(tasks);
   return (
     <Layout>
-      {isLoading && <p>Loading tasks...</p>}
-      {error && <p>{error}</p>}
-      <p>{items.length > 0 && JSON.stringify(items, null, 2)}</p>
-      {/* <AppBar />
+      {/* {isLoading && <p>Loading tasks...</p>}
+      {error && <p>{error}</p>} */}
+      {/* <p>{items.length > 0 && JSON.stringify(items, null, 2)}</p> */}
+      <AppBar />
       <TaskForm />
       {isLoading && <p>Loading tasks...</p>}
       {error && <p>{error}</p>}
-      <p>{items.length > 0 && JSON.stringify(items, null, 2)}</p>
-      <TaskList /> */}
+      {/* <p>{items.length > 0 && JSON.stringify(items, null, 2)}</p> */}
+      <TaskList />
     </Layout>
   );
 };
