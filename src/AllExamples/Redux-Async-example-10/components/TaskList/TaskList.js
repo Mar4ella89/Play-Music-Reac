d6d4fc9337e1034a803b3e3cc88f193064132692
Marat Baikin <3371335@gmail.com>
2023-07-25 17:30:44 +0300

    refactoring selectVisibleTasks

--- src/AllExamples/Redux-Async-example-10/components/TaskList/TaskList.js
+++ src/AllExamples/Redux-Async-example-10/components/TaskList/TaskList.js
@@ -1,30 +1,23 @@
 import { useSelector } from 'react-redux';
 
-import { selectTasks, selectStatusFilter } from '../../redux/selectors';
+import {
+  selectTasks,
+  selectStatusFilter,
+  selectVisibleTasks,
+} from '../../redux/selectors';
 import { Task } from '../Task/Task';
 // Импортируем объект значений фильтра
 import { statusFilters } from '../../redux/constants';
 
 import css from './TaskList.module.css';
 
-const getVisibleTasks = (tasks, statusFilter) => {
-  switch (statusFilter) {
-    case statusFilters.active:
-      return tasks.filter(task => !task.completed);
-    case statusFilters.completed:
-      return tasks.filter(task => task.completed);
-    default:
-      return tasks;
-  }
-};
-
 export const TaskList = () => {
   // Получаем массив задач из состояния Redux
-  const tasks = useSelector(selectTasks);
+  // const tasks = useSelector(selectTasks);
   // Получаем значение фильтра из состояния Redux
-  const statusFilter = useSelector(selectStatusFilter);
+  // const statusFilter = useSelector(selectStatusFilter);
   // Вычисляем массив задач которые необходимо отображать в интерфейсе
-  const visibleTasks = getVisibleTasks(tasks, statusFilter);
+  const visibleTasks = useSelector(selectVisibleTasks);
 
   return (
     <ul className={css.list}>
