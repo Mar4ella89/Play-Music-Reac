85c9f5f5e798588015414ab99881a888da8b1872
Marat Baikin <3371335@gmail.com>
2023-07-25 17:34:02 +0300

    Update TaskList.js

--- src/AllExamples/Redux-Async-example-10/components/TaskList/TaskList.js
+++ src/AllExamples/Redux-Async-example-10/components/TaskList/TaskList.js
@@ -1,22 +1,11 @@
 import { useSelector } from 'react-redux';
 
-import {
-  selectTasks,
-  selectStatusFilter,
-  selectVisibleTasks,
-} from '../../redux/selectors';
+import { selectVisibleTasks } from '../../redux/selectors';
 import { Task } from '../Task/Task';
-// Импортируем объект значений фильтра
-import { statusFilters } from '../../redux/constants';
 
 import css from './TaskList.module.css';
 
 export const TaskList = () => {
-  // Получаем массив задач из состояния Redux
-  // const tasks = useSelector(selectTasks);
-  // Получаем значение фильтра из состояния Redux
-  // const statusFilter = useSelector(selectStatusFilter);
-  // Вычисляем массив задач которые необходимо отображать в интерфейсе
   const visibleTasks = useSelector(selectVisibleTasks);
 
   return (
