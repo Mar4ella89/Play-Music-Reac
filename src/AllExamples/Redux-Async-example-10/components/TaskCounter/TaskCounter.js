c36fff86ae4ef3e748223a7be8afc5a959e203d0
Marat Baikin <3371335@gmail.com>
2023-07-25 17:59:25 +0300

    updated TaskCounter

--- src/AllExamples/Redux-Async-example-10/components/TaskCounter/TaskCounter.js
+++ src/AllExamples/Redux-Async-example-10/components/TaskCounter/TaskCounter.js
@@ -1,29 +1,15 @@
-// Импортируем хук
 import { useSelector } from 'react-redux';
 
-import { selectTasks } from '../../redux/selectors';
+import { selectTaskCount } from '../../redux/selectors';
 
 import css from './TaskCounter.module.css';
 
 export const TaskCounter = () => {
-  // Получаем массив задач из состояния Redux
-  const tasks = useSelector(selectTasks);
-  // На базе состояния Redux получаем производные данные
-  const count = tasks.reduce(
-    (acc, task) => {
-      if (task.completed) {
-        acc.completed += 1;
-      } else {
-        acc.active += 1;
-      }
-      return acc;
-    },
-    { active: 0, completed: 0 }
-  );
+  const { active, completed } = useSelector(selectTaskCount);
   return (
     <div>
-      <p className={css.text}>Active: {count.active}</p>
-      <p className={css.text}>Completed: {count.completed}</p>
+      <p className={css.text}>Active: {active}</p>
+      <p className={css.text}>Completed: {completed}</p>
     </div>
   );
 };
