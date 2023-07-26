597c6274b471182d85852e9f7941efc4f6a41ff4
Marat Baikin <3371335@gmail.com>
2023-07-26 15:23:21 +0300

    Update selectors.js

--- src/AllExamples/Redux-Async-example-10/redux/selectors.js
+++ src/AllExamples/Redux-Async-example-10/redux/selectors.js
@@ -1,3 +1,5 @@
+import { createSelector } from '@reduxjs/toolkit';
+
 import { statusFilters } from './constants';
 
 export const selectTasks = state => state.tasks.items;
@@ -8,23 +10,22 @@ export const selectError = state => state.tasks.error;
 
 export const selectStatusFilter = state => state.filters.status;
 
-export const selectVisibleTasks = state => {
-  const tasks = selectTasks(state);
-  const statusFilter = selectStatusFilter(state);
-
-  switch (statusFilter) {
-    case statusFilters.active:
-      return tasks.filter(task => !task.completed);
-    case statusFilters.completed:
-      return tasks.filter(task => task.completed);
-    default:
-      return tasks;
+export const selectVisibleTasks = createSelector(
+  [selectTasks, selectStatusFilter],
+  (tasks, statusFilter) => {
+    console.log('Calculating visible tasks. Now memoized!');
+    switch (statusFilter) {
+      case statusFilters.active:
+        return tasks.filter(task => !task.completed);
+      case statusFilters.completed:
+        return tasks.filter(task => task.completed);
+      default:
+        return tasks;
+    }
   }
-};
+);
 
-export const selectTaskCount = state => {
-  const tasks = selectTasks(state);
-
+export const selectTaskCount = createSelector([selectTasks], tasks => {
   return tasks.reduce(
     (count, task) => {
       if (task.completed) {
@@ -36,4 +37,4 @@ export const selectTaskCount = state => {
     },
     { active: 0, completed: 0 }
   );
-};
+});
