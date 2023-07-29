8c9599f770bae4e06fb872f647ac72ca112ee1df
Marat Baikin <3371335@gmail.com>
2023-07-29 12:23:45 +0300

    updated taskSlice

--- src/AllExamples/Redux-Auth-example-11/redux/tasks/slice.js
+++ src/AllExamples/Redux-Auth-example-11/redux/tasks/slice.js
@@ -54,38 +54,6 @@ const tasksSlice = createSlice({
         state.items.splice(index, 1, action.payload);
       })
       .addCase(toggleCompleted.rejected, handleRejected);
-    // [fetchTasks.pending]: handlePending,
-    // [fetchTasks.fulfilled](state, action) {
-    //   state.isLoading = false;
-    //   state.error = null;
-    //   state.items = action.payload;
-    // },
-    // [fetchTasks.rejected]: handleRejected,
-    // [addTask.pending]: handlePending,
-    // [addTask.fulfilled](state, action) {
-    //   state.isLoading = false;
-    //   state.error = null;
-    //   state.items.push(action.payload);
-    // },
-    // [addTask.rejected]: handleRejected,
-    // [deleteTask.pending]: handlePending,
-    // [deleteTask.fulfilled](state, action) {
-    //   state.isLoading = false;
-    //   state.error = null;
-    //   const index = state.items.findIndex(task => task.id === action.payload);
-    //   state.items.splice(index, 1);
-    // },
-    // [deleteTask.rejected]: handleRejected,
-    // [toggleCompleted.pending]: handlePending,
-    // [toggleCompleted.fulfilled](state, action) {
-    //   state.isLoading = false;
-    //   state.error = null;
-    //   const index = state.items.findIndex(
-    //     task => task.id === action.payload.id
-    //   );
-    //   state.items.splice(index, 1, action.payload);
-    // },
-    // [toggleCompleted.rejected]: handleRejected,
   },
 });
 
