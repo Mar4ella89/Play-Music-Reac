c4dcbd9ccee8d65afd0a12073c82d2f6cfef0265
Marat Baikin <3371335@gmail.com>
2023-07-29 11:10:24 +0300

    Update Task.js and created UserMenu

---
+++ src/AllExamples/Redux-Auth-example-11/pages/Tasks.js
@@ -0,0 +1,32 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Helmet } from 'react-helmet';
+import { TaskList } from '../components/TaskList/TaskList';
+import { TaskForm } from '../components/TaskForm/TaskForm';
+import { StatusFilter } from '../components/StatusFilter/StatusFilter';
+import { AppBar } from '../components/AppBar/AppBar';
+import { Layout } from '../components/Layout/Layout';
+import { fetchTasks } from '../redux/tasks/operations';
+import { selectLoading } from '../redux/tasks/selectors';
+
+export default function Tasks() {
+  const dispatch = useDispatch();
+  const isLoading = useSelector(selectLoading);
+
+  useEffect(() => {
+    dispatch(fetchTasks());
+  }, [dispatch]);
+
+  return (
+    <Layout>
+      <Helmet>
+        <title>Your tasks</title>
+      </Helmet>
+      <AppBar />
+      <StatusFilter />
+      <TaskForm />
+      <div>{isLoading && 'Request in progress...'}</div>
+      <TaskList />
+    </Layout>
+  );
+}
