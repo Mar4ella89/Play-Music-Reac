197ba98124f279e78a1348ea5040e4c54cabb663
Marat Baikin <3371335@gmail.com>
2023-08-03 17:27:17 +0300

    Update ReduxAuthExample.jsx

--- src/AllExamples/Redux-Auth-example-11/ReduxAuthExample.jsx
+++ src/AllExamples/Redux-Auth-example-11/ReduxAuthExample.jsx
@@ -1,19 +1,15 @@
-import { BrowserRouter } from 'react-router-dom';
 import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
 import { store, persistor } from './redux/store';
 
 import { App } from './components/App.js';
-// import 'modern-normalize';
-// import './styles.css';
+import 'modern-normalize';
 
 const ReduxAuthExample = () => {
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        {/* <BrowserRouter> */}
         <App />
-        {/* </BrowserRouter> */}
       </PersistGate>
     </Provider>
   );
